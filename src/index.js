1912d48832e1b4d52cc8ad722cecf29b1fef642c
adolivaresl <domioli@gmail.com>
2020-05-14 19:52:42 -0400

    i add first pages with navbar, and navbar complet

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const element = <h1>Hello, Platzi Badges!</h1>;
+import 'bootstrap/dist/css/bootstrap.css';
+import './global.css';
+// import Badge from './components/Badge';
+// import Navbar from './components/Navbar';
+import BadgeNew from './pages/BadgeNew';
+
 
 const container = document.getElementById('app');
 
-// ReactDOM.render(__qué__, __dónde__);
-ReactDOM.render(element, container);
+// ReactDOM.render(__qué quiero mostrar__, __dónde_quiero mostrar_);
+ReactDOM.render(<BadgeNew />, container);

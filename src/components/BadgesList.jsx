77f0918924123cb6ba2cc12cbd547812e0e0602b
adolivaresl <domioli@gmail.com>
2020-07-11 17:42:57 -0400

    i add searcj filter with hooks

--- src/components/BadgesList.jsx
+++ src/components/BadgesList.jsx
@@ -5,20 +5,66 @@ import './styles/BadgesList.css';
 import twitter from '../images/social-media.svg'
 
 
-class BadgesList extends React.Component{
-  render() {
-    if (this.props.badges.length === 0) {
+function useSearchBadges(badges) {
+  const [ query, setQuery ] = React.useState ('');
+
+  // este filtro es simple cuando son unos pocos, pero si la lista es de miles este calculo es un poco costoso, hara un cuello de botella, para solucionarlo usaremos un hooks que es, usememo
+  // const filteredBadges= badges.filter(badge => {
+  //   return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase());
+  // })
+const [filteredBadges, setFilteredBadges ] = React.useState(badges);
+
+  React.useMemo(()=> {
+    const result = badges.filter(badge => {
+    // ponemos tolowercase para normalizar y todo convertirlño en minuscula ypara que busque en nombre y apellido creamos un string badge. first o last
+    return `${badge.firstName} ${badge.lastName}`.toLowerCase().includes(query.toLowerCase());
+  });
+
+setFilteredBadges(result)
+}, [badges, query]);
+
+return { query, setQuery, filteredBadges}
+}
+
+function BadgesList  (props) {
+  const badges = props.badges;
+
+  const { query, setQuery, filteredBadges } = useSearchBadges(badges)
+
+
+  if (filteredBadges.length === 0) {
       return (
         <div>
+          <div className="form-group">
+        <label>Filter Badges</label>
+        {/* pasamos valos al input para ser controlado, necesitamos que cada vez que se escriba guardar el valor que hay asi despues lo podemos utilizar, a traves de dos props con el value y el onChage  */}
+        <input type="text" className="form-control"
+        value={query}
+        onChange = {(e) => {
+          setQuery(e.target.value);
+        }}
+        />
+      </div>
           <h3>No badge were found</h3>
           <Link className="btn btn-primary" to="/badges/new"> Create new badge</Link>
         </div>
       )
     }
     return(
+      <div className="BadgesList">
+      <div className="form-group">
+        <label>Filter Badges</label>
+        {/* pasamos valos al input para ser controlado, necesitamos que cada vez que se escriba guardar el valor que hay asi despues lo podemos utilizar, a traves de dos props con el value y el onChage  */}
+        <input type="text" className="form-control"
+        value={query}
+        onChange = {(e) => {
+          setQuery(e.target.value);
+        }}
+        />
+      </div>
       <ul className="list-unstyled BadgeList">
         {/* ya no viene de state , {this.state.dat.map((badge) => {,  si no que de props. badges */}
-        {this.props.badges.map((badge) => {
+        {filteredBadges.map((badge) => {
           return (
             
             <li className="BadgesListItem" key={badge.id}>
@@ -36,9 +82,8 @@ class BadgesList extends React.Component{
           );
         })}
       </ul>      
-    
+      </div>
     );
   }
-}
 
 export default BadgesList;
